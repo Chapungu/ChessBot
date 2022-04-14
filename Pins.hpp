33874978a6c9bf57faa41b20c7a7673e4cdc3769
Charlie <charlie.saunders1@gmail.com>
2022-04-14 15:11:25 +0100

    Initial commit

---
+++ Pins.hpp
@@ -0,0 +1,30 @@
+
+#ifndef Pins_hpp
+#define Pins_hpp
+#include "mbed.h"
+
+
+typedef struct {
+    PinName Enable;
+    PinName Direction;
+    PinName Step; 
+} StepperPins;
+
+const StepperPins xAxisPins{
+    PD_14, //Enable     D10
+    PD_15, //Direction  D9
+    PF_12  //Step       D8
+};
+
+const StepperPins yAxisPins{
+    PF_13, //Enable     D7
+    PE_9,  //Direction  D6
+    PE_11  //Step       D5
+};
+
+const StepperPins zAxisPins{
+    PF_14, //Enable     D4
+    PE_13, //Direction  D3
+    PF_15  //Step       D2
+};
+#endif
