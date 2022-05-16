9224b1c7d0aea5ddb2ede6a80b7110ec89977f1a
Charlie <charlie.saunders1@gmail.com>
2022-05-16 20:13:56 +0100

    Added Buffer for serial

---
+++ GameManipulatorRobot/buffer.h
@@ -0,0 +1,52 @@
+#ifndef _BUFFER_H_
+#define _BUFFER_H_
+#include "mbed.h"
+#include <cstdint>
+
+#define BUFFER_CAPACITY 32
+
+class Buffer{
+    public:
+    Buffer(){
+        head = 0;//pointer to start reading data
+        tail = head; //pointer to stop reading data
+        size = 0;//how much data. 
+    }
+    void put(uint8_t data){
+
+        if (size >= BUFFER_CAPACITY){
+            //printf("No Space in buffer\r\n");
+            return;
+        }else{
+            buff[head] = data; //put data at head then increment head
+            head = ((head + 1) % BUFFER_CAPACITY);
+            size++;
+        }
+
+    }
+    uint8_t pop(){
+        uint8_t data = 0;
+
+        if(size <= 0){
+            printf("Buffer Underrun\r\n");
+        }else{
+            uint8_t data = buff[tail];
+            tail = ((tail + 1) % BUFFER_CAPACITY);
+            size--;
+        }
+
+        return data;
+    }
+
+    uint32_t getsize(){
+        return size;
+    }
+
+    private:
+        uint8_t buff[BUFFER_CAPACITY];
+        uint8_t head;
+        uint8_t tail;
+        uint32_t size;
+};
+
+#endif
--- GameManipulatorRobot/main.cpp
+++ GameManipulatorRobot/main.cpp
@@ -3,10 +3,15 @@
 #include "Pins.hpp"
 #include "ReadabilityDefinitions.hpp"
 #include "coordinates.hpp"
+#include "iostream"
+#include "buffer.h"
+#include <cstdint>
 
 // main() runs in its own thread in the OS
 
+UnbufferedSerial control(USBTX,USBRX,115200);
 
+Buffer coordBuff;
 
 DigitalOut Green(LED1);
 DigitalOut Yellow(LED2);
@@ -20,22 +25,30 @@ Thread StepX, StepY, StepZ;
 void Xaxis_thread();
 void Yaxis_thread();
 void Zaxis_thread();
-
+void getCoords();
 
 
 
 int main()
 {   
-    
-    
-
     printf("in Main \r\n");
+    /*
+    control.attach(getCoords, SerialBase::RxIrq);
+    while(1){
+    while(coordBuff.getsize()!=0){
+        uint8_t check = coordBuff.pop();
+        printf("Your input was %c\r\n", check);
+    }
+    }
+    */
     
 
 
     StepX.start(Xaxis_thread);
-    StepY.start(Yaxis_thread);
+    //StepY.start(Yaxis_thread);
     //StepZ.start(Zaxis_thread);
+
+    xaxis.move(1,80);
 
 
 
@@ -48,6 +61,12 @@ int main()
 
 } 
 
+void getCoords(){
+    char d;                 //provide plae to put charachter
+    control.read(&d, 1);    //read serial data and copy data to d
+ 
+    coordBuff.put(d);
+}
 
 void Xaxis_thread(){
     
